e426a83028178ff98ab97d2bb8c455e652505bb2
[dyad] <contact@gosgconsulting.com>
2025-12-24 02:10:33 +0700

    [dyad] add Dyad component tagger

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import dyadComponentTagger from '@dyad-sh/react-vite-component-tagger';
 
 
 // https://vitejs.dev/config/
@@ -17,7 +18,7 @@ export default defineConfig(({ mode }) => ({
       }
     }
   },
-  plugins: [
+  plugins: [dyadComponentTagger(), 
     react(),
     mode === 'development' &&
     componentTagger(),
